89f8a44c1593bd44170623d7ce3733ce4533ac21
Lam Chee Choong <lamcc@Lams-MacBook-Pro.local>
2022-11-03 22:09:22 +0800

    batch download

--- exec.py
+++ exec.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import command
+import re
 
-page = 1
-while page < 2:
+page = 3
+
+while page <= 140:
     url = "http://world.kbs.co.kr/service/program_listenagain.htm?page=" + str(page) + "&lang=e&procode=parkofd&segcode="
+    print(url)
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
     mp3 = soup.select('a.download')
@@ -13,11 +15,16 @@ while page < 2:
         for el in mp3:
             href = el.get('href')
             href = href.strip()
-            #http://world.kbs.co.kr/down.htm?inpage_id=61369&Type=MP3
-            cmd = "wget http://world.kbs.co.kr" + href
-            r = command.run([cmd])
-            page += 1
-    else:
-        page = 999999
-    
+            #http://world.kbs.co.kr/"down.htm?inpage_id=61369&Type=MP3
+            hostname = "http://world.kbs.co.kr/"
+            furl = hostname + href
+            r = requests.get(furl)
+            d = r.headers['content-disposition']
+            fname = re.findall("filename=(.+)", d)[0]
+
+            open(fname, "wb").write(r.content)
+            print(fname)
+
+    page += 1
+
     
